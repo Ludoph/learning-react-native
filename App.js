5a7bbc5256d5acf44b7f68e85d95f96f7567106c
phounphonh.ludovic@gmail.com <phounphonh.ludovic@gmail.com>
2023-07-03 21:40:19 +0200

    add feature save img

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
+import * as MediaLibrary from "expo-media-library";
+
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 
@@ -12,10 +14,14 @@ import EmojiList from "./components/EmojiList";
 import EmojiSticker from "./components/EmojiSticker";
 
 import * as ImagePicker from "expo-image-picker";
+import { captureRef } from "react-native-view-shot";
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
+  const imageRef = useRef();
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+
   const [pickedEmoji, setPickedEmoji] = useState(null);
   const [selectedImage, setSelectedImage] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -46,47 +52,71 @@ export default function App() {
     setIsModalVisible(false);
   };
 
-  const onSaveImageAsync = async () => {};
+  const onSaveImageAsync = async () => {
+    try {
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1,
+      });
+
+      await MediaLibrary.saveToLibraryAsync(localUri);
+      if (localUri) {
+        alert("Saved!");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  if (status === null) {
+    requestPermission();
+  }
+
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.container}>
         <View style={styles.imageContainer}>
-          <ImageViewer
-            placeholderImageSource={PlaceholderImage}
-            selectedImage={selectedImage}
-          />
-          {pickedEmoji !== null ? (
-            <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
-          ) : null}
-          {showAppOptions ? (
-            <View style={styles.optionsContainer}>
-              <View style={styles.optionsRow}>
-                <IconButton icon="refresh" label="Reset" onPress={onReset} />
-                <CircleButton onPress={onAddSticker} />
-                <IconButton
-                  icon="save-alt"
-                  label="Save"
-                  onPress={onSaveImageAsync}
+          <View ref={imageRef} collapsable={false}>
+            <ImageViewer
+              placeholderImageSource={PlaceholderImage}
+              selectedImage={selectedImage}
+            />
+            {pickedEmoji !== null ? (
+              <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+            ) : null}
+            {showAppOptions ? (
+              <View style={styles.optionsContainer}>
+                <View style={styles.optionsRow}>
+                  <IconButton icon="refresh" label="Reset" onPress={onReset} />
+                  <CircleButton onPress={onAddSticker} />
+                  <IconButton
+                    icon="save-alt"
+                    label="Save"
+                    onPress={onSaveImageAsync}
+                  />
+                </View>
+              </View>
+            ) : (
+              <View style={styles.footerContainer}>
+                <Button
+                  theme="primary"
+                  label="Choose a photo"
+                  onPress={pickImageAsync}
+                />
+                <Button
+                  label="Use this photo"
+                  onPress={() => setShowAppOptions(true)}
                 />
               </View>
-            </View>
-          ) : (
-            <View style={styles.footerContainer}>
-              <Button
-                theme="primary"
-                label="Choose a photo"
-                onPress={pickImageAsync}
+            )}
+            <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+              <EmojiList
+                onSelect={setPickedEmoji}
+                onCloseModal={onModalClose}
               />
-              <Button
-                label="Use this photo"
-                onPress={() => setShowAppOptions(true)}
-              />
-            </View>
-          )}
-          <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-            <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
-          </EmojiPicker>
-          <StatusBar style="auto" />
+            </EmojiPicker>
+            <StatusBar style="auto" />
+          </View>
         </View>
       </View>
     </GestureHandlerRootView>
--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,15 +1,22 @@
 import { View, Image } from "react-native";
-import { TapGestureHandler } from "react-native-gesture-handler";
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
   useAnimatedGestureHandler,
   withSpring,
 } from "react-native-reanimated";
+import {
+  PanGestureHandler,
+  TapGestureHandler,
+} from "react-native-gesture-handler";
 
 const AnimatedImage = Animated.createAnimatedComponent(Image);
+const AnimatedView = Animated.createAnimatedComponent(View);
 
 export default function EmojiSticker({ imageSize, stickerSource }) {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
   const scaleImage = useSharedValue(imageSize);
   const onDoubleTap = useAnimatedGestureHandler({
     onActive: () => {
@@ -26,15 +33,41 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
     };
   });
 
+  const onDrag = useAnimatedGestureHandler({
+    onStart: (event, context) => {
+      context.translateX = translateX.value;
+      context.translateY = translateY.value;
+    },
+    onActive: (event, context) => {
+      translateX.value = event.translationX + context.translateX;
+      translateY.value = event.translationY + context.translateY;
+    },
+  });
+
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
+    };
+  });
+
   return (
-    <View style={{ top: -350 }}>
-      <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
-        <AnimatedImage
-          source={stickerSource}
-          resizeMode="contain"
-          style={[imageStyle, { width: imageSize, height: imageSize }]}
-        />
-      </TapGestureHandler>
-    </View>
+    <PanGestureHandler onGestureEvent={onDrag}>
+      <AnimatedView style={[containerStyle, { top: -350 }]}>
+        <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
+          <AnimatedImage
+            source={stickerSource}
+            resizeMode="contain"
+            style={[imageStyle, { width: imageSize, height: imageSize }]}
+          />
+        </TapGestureHandler>
+      </AnimatedView>
+    </PanGestureHandler>
   );
 }
